5782859463bda9b0a2556142a3a38d659b6cc7ef
prince-harry-2813 <54502376+prince-harry-2813@users.noreply.github.com>
2019-09-24 18:55:11 +0300

    nowwwww

--- portal.js
+++ portal.js
@@ -1,51 +1,24 @@
 let caftor= document.getElementById("plus").addEventListener("click",()=>{window.location.href="https://eloquentjavascript.net/index.html"})
 filtercategory("all")
-function filtercategory(c) {
-    var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+function filtercategory(c) { 
+  if(c=="all") {
+    var a = document.getElementById("list").getElementsByClassName("filterDiv");
+    for (let i = 0; i < a.length; i++) 
+    {
+    a[i].style.display="flex";
+    } 
+    return;
   }
-} 
-function AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-    }
+  var x = document.getElementById("list").getElementsByClassName("filterDiv");
+  for (let i = 0; i < x.length; i++) 
+  {
+    x[i].style.display = "none";
   }
   
-  function RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1);     
-      }
-    }
-    element.className = arr1.join(" ");
-  }
-  function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1);     
-      }
-    }
-    element.className = arr1.join(" ");
-  }
-  var btnContainer = document.getElementById("myBtnContainer");
-  var btns = btnContainer.getElementsByClassName("btn");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function(){
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
-  }
+ var s = document.getElementById("list").getElementsByClassName(c);
+ for (let i = 0; i < s.length; i++) 
+ {
+ s[i].style.display="flex";
+ }  
+ 
+}
