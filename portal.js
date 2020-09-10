418cae60992118b95d2f3cf7654d38d15b8ac92c
prince-harry-2813 <54502376+prince-harry-2813@users.noreply.github.com>
2020-09-10 23:49:03 +0300

    update

--- portal.js
+++ portal.js
@@ -1,5 +1,11 @@
 let caftor= document.getElementById("plus").addEventListener("click",()=>{window.location.href="https://eloquentjavascript.net/index.html"})
 function filtercategory(c) { 
+
+  var x = document.getElementById("list").getElementsByClassName("filterDiv");
+  for (let i = 0; i < x.length; i++) 
+  {
+    x[i].style.display = "none";
+  }
   if(c=="all") {
     var a = document.getElementById("list").getElementsByClassName("filterDiv");
     for (let i = 0; i < a.length; i++) 
@@ -8,16 +14,11 @@ function filtercategory(c) {
     } 
     return;
   }
-  var x = document.getElementById("list").getElementsByClassName("filterDiv");
-  for (let i = 0; i < x.length; i++) 
-  {
-    x[i].style.display = "none";
-  }
-  
+  else{
  var s = document.getElementById("list").getElementsByClassName(c);
  for (let i = 0; i < s.length; i++) 
  {
  s[i].style.display="block";
  }  
- 
+}
 }
